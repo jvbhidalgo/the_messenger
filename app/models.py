2725aca3d67aff27bd4d596174101135e8d5bd1d
jvbhidalgo <jvbhidalgo97@gmail.com>
2020-05-12 21:55:29 -0300

    Att

--- app/models.py
+++ app/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User #oneToMany
-
-class Post(models.Model):
-    title = models.CharField(max_length=60)
-    conteudo = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    autor = models.ForeignKey(User, on_delete=models.CASCADE)
-
--- app/views.py
+++ app/views.py
@@ -5,8 +5,15 @@ from django.contrib.auth.models import User
 
 def home(request):
 
+    pesquisa = User.objects.filter(username__contains='pattern')
     users = User.objects.all().order_by('username')
 
-    context = {'users': users}
+    data = {}
+    data['users'] = []
+    for i in users:
+        if i.id != request.user.id:
+            data['users'].append(i)
 
-    return render(request,'app/home.html', context)
+    #context = {'users': users}
+
+    return render(request,'app/home.html', data)
